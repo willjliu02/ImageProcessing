a1c2c0f930e43d465157d4cbe5a2275ec12aa2e2
willjliu02 <william.liu1107@gmail.com>
2022-11-03 02:37:50 -0400

    Implemented the controller.

--- src/view/IView.java
+++ src/view/IView.java
@@ -1,4 +1,15 @@
 package view;
 
+import java.io.IOException;
+
+/**
+ * Represents a view of the controller.
+ */
 public interface IView {
+  /**
+   * Renders a message to the view.
+   * @param message the message to be written to the view
+   * @throws IOException thrown if unable to render the message
+   */
+  public void renderMessage(String message) throws IOException;
 }
